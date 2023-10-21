128310d4b5a94dd5d581699e686d69685de3715b
Leandro Klyte <klyte45@hotmail.com>
2023-10-21 17:58:43 -0300

    v0.0.2r2 Fixing mod path getter

--- BelzontAdr/AddressesCs2Mod.cs
+++ BelzontAdr/AddressesCs2Mod.cs
@@ -5,7 +5,7 @@ using Game.UI.Menu;
 using System.Collections.Generic;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.2.1")]
+[assembly: AssemblyVersion("0.0.2.2")]
 namespace BelzontAdr
 {
     public class AddressesCs2Mod : BasicIMod<AdrModData>, IMod
