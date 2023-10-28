f916be59c5297e1b77749e7ed83588d995026d45
Leandro Klyte <klyte45@hotmail.com>
2023-10-28 13:20:14 -0300

    v0.0.2r3 Fixing UI pixel density

--- BelzontAdr/AddressesCs2Mod.cs
+++ BelzontAdr/AddressesCs2Mod.cs
@@ -5,7 +5,7 @@ using Game.UI.Menu;
 using System.Collections.Generic;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.2.2")]
+[assembly: AssemblyVersion("0.0.2.3")]
 namespace BelzontAdr
 {
     public class AddressesCs2Mod : BasicIMod<AdrModData>, IMod
